5aa852a051261aef3203ccb6b34b4922383432e6
Javier Cicchelli <javier@rock-n-code.com>
2024-09-07 07:46:59 +0000

    Basic package setup (#1)
    This PR contains the work done to setup this Swift package, so development can start from here.
    
    Reviewed-on: https://repo.rock-n-code.com/rock-n-code/amiibo-api/pulls/1
    Co-authored-by: Javier Cicchelli <javier@rock-n-code.com>
    Co-committed-by: Javier Cicchelli <javier@rock-n-code.com>

---
+++ Makefile
@@ -0,0 +1,42 @@
+# ENVIRONMENT VARIABLES
+
+environment ?= .env
+
+include $(environment)
+export $(shell sed 's/=.*//' $(environment))
+
+# IDE
+
+open-in-xcode: ## Opens this package with Xcode
+	@open -a Xcode Package.swift
+
+open-in-vscode: ## Opens this package with Visual Studio Code
+	@code .
+
+# SWIFT PACKAGE MANAGER
+
+package-build: ## Builds the project locally
+	@swift build
+
+package-clean: ## Deletes built SPM artifacts from the package
+	@swift package clean
+
+package-outdated: ## Lists the SPM package dependencies that can be updated
+	@swift package update --dry-run
+
+package-reset: ## Resets the complete SPM cache/build folder from the package
+	@swift package reset
+
+package-update: ## Updates the SPM package dependencies
+	@swift package update
+
+# HELP
+
+# Output the documentation for each of the defined tasks when `help` is called.
+# Reference: https://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
+.PHONY: help
+
+help: ## Prints the written documentation for all the defined tasks
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+.DEFAULT_GOAL := help
--- Package.swift
+++ Package.swift
@@ -1,24 +1,36 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
-    name: "amiibo-api",
+    name: AmiiboAPI.package,
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "amiibo-api",
-            targets: ["amiibo-api"]),
+            name: AmiiboAPI.package,
+            targets: [
+                AmiiboAPI.target
+            ]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "amiibo-api"),
+            name: AmiiboAPI.target,
+            path: "Sources"
+        ),
         .testTarget(
-            name: "amiibo-apiTests",
-            dependencies: ["amiibo-api"]
+            name: AmiiboAPI.test,
+            dependencies: [
+                .byName(name: AmiiboAPI.target)
+            ],
+            path: "Tests"
         ),
     ]
 )
+
+// MARK: - Constants
+
+enum AmiiboAPI {
+    static let package = "amiibo-api"
+    static let target = "AmiiboAPI"
+    static let test = "\(AmiiboAPI.target)Tests"
+}
